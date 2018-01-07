d65a2716a67296d6321c9ece55cf80de7c6a2617
Matthew Daly <matthewbdaly@gmail.com>
2018-01-07 18:30:41 +0000

    Initial commit

---
+++ tests/SimpleTestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tests;
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
+class SimpleTestCase extends PHPUnit\Framework\TestCase
+{
+    use MockeryPHPUnitIntegration;
+}
