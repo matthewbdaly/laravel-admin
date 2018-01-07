3497b4ca66d13cc11fb505470ab14c7bc7c0e0fe
Matthew Daly <matthewbdaly@gmail.com>
2018-01-07 19:25:53 +0000

    Now specify full namespaces in routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 // Authentication Routes...
-Route::get('admin/login', 'Auth\LoginController@showLoginForm')->name('admin.login');
-Route::post('admin/login', 'Auth\LoginController@login');
-Route::post('admin/logout', 'Auth\LoginController@logout')->name('admin.logout');
+Route::get('admin/login', 'Matthewbdaly\LaravelAdmin\Http\Controllers\Auth\LoginController@showLoginForm')->name('admin.login');
+Route::post('admin/login', 'Matthewbdaly\LaravelAdmin\Http\Controllers\Auth\LoginController@login');
+Route::post('admin/logout', 'Matthewbdaly\LaravelAdmin\Http\Controllers\Auth\LoginController@logout')->name('admin.logout');
 
 // Admin home page
-Route::get('/admin', 'AdminControlelr@index')->name('admin/home')->middleware('admin');
+Route::get('/admin', 'AdminController@index')->name('admin/home')->middleware('admin');
