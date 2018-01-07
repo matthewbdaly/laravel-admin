645ccd5ed3436e0225ffdb983e061a4d2afa692b
Matthew Daly <matthewbdaly@gmail.com>
2018-01-07 21:02:16 +0000

    Fixed a problem with the routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
-// Authentication Routes...
-Route::get('admin/login', 'Matthewbdaly\LaravelAdmin\Http\Controllers\Auth\LoginController@showLoginForm')->name('admin.login');
-Route::post('admin/login', 'Matthewbdaly\LaravelAdmin\Http\Controllers\Auth\LoginController@login');
-Route::post('admin/logout', 'Matthewbdaly\LaravelAdmin\Http\Controllers\Auth\LoginController@logout')->name('admin.logout');
 
-// Admin home page
-Route::get('/admin', 'Matthewbdaly\LaravelAdmin\Http\Controllers\AdminController@index')->name('admin.home')->middleware('admin');
+// Needs web middleware
+Route::group(['middleware' => ['web']], function () {
+
+    // Authentication Routes...
+    Route::get('admin/login', 'Matthewbdaly\LaravelAdmin\Http\Controllers\Auth\LoginController@showLoginForm')->name('admin.login');
+    Route::post('admin/login', 'Matthewbdaly\LaravelAdmin\Http\Controllers\Auth\LoginController@login');
+    Route::post('admin/logout', 'Matthewbdaly\LaravelAdmin\Http\Controllers\Auth\LoginController@logout')->name('admin.logout');
+
+    // Admin home page
+    Route::get('/admin', 'Matthewbdaly\LaravelAdmin\Http\Controllers\AdminController@index')->name('admin.home')->middleware('admin');
+});
