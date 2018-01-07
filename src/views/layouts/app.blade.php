f8ccd3dc90dc28f1255268bba5ef59406579b5dc
Matthew Daly <matthewbdaly@gmail.com>
2018-01-07 21:46:21 +0000

    Amended static file names

--- src/views/layouts/app.blade.php
+++ src/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -74,6 +74,6 @@
     </div>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/admin.js') }}"></script>
 </body>
 </html>
