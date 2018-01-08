c7370392f02c338fb6c378f3cbccca1c298f303a
Matthew Daly <matthewbdaly@gmail.com>
2018-01-08 18:10:29 +0000

    Copied over and refactored routes

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,18 @@ Route::group(['middleware' => ['web']], function () {
     Route::post('admin/logout', 'Matthewbdaly\LaravelAdmin\Http\Controllers\Auth\LoginController@logout')->name('admin.logout');
 
     // Admin home page
-    Route::get('/admin', 'Matthewbdaly\LaravelAdmin\Http\Controllers\AdminController@index')->name('admin.home')->middleware('admin');
+    Route::group(['middleware' => ['admin']], function () {
+        Route::get('/admin', 'Matthewbdaly\LaravelAdmin\Http\Controllers\AdminController@index')->name('admin.home');
+
+        // Resource routes
+        Route::group(['middleware' => ['admin_model_exists']], function () {
+            Route::get('/admin/{resource}', 'AdminResourceController@index')->name('admin.resource');
+            Route::get('/admin/{resource}/create', 'AdminResourceController@create')->name('admin.resource.create');
+            Route::post('/admin/{resource}', 'AdminResourceController@store');
+            Route::get('/admin/{resource}/{id}', 'AdminResourceController@show')->name('admin.resource.show');
+            Route::put('/admin/{resource}/{id}', 'AdminResourceController@update')->name('admin.resource.update');
+            Route::patch('/admin/{resource}/{id}', 'AdminResourceController@update');
+            Route::delete('/admin/{resource}/{id}', 'AdminResourceController@destroy')->name('admin.resource.delete');
+        });
+    });
 });
