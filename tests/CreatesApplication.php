b9a51db985a939723e1f4c5dd3ac6c1a05a2034c
Matthew Daly <matthewbdaly@gmail.com>
2018-01-11 11:27:44 +0000

    Added a BrowserKit test case

---
+++ tests/CreatesApplication.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests;
+
+use Hash;
+
+trait CreatesApplication
+{
+	protected function getPackageProviders($app)
+	{
+		return ['Matthewbdaly\LaravelAdmin\Providers\ServiceProvider'];
+	}
+
+    /**
+     * Creates the application.
+     *
+     * Needs to be implemented by subclasses.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = parent::createApplication();
+
+        Hash::setRounds(4);
+
+        return $app;
+    }
+}
